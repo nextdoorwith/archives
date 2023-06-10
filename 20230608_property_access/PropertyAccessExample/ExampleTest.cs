a0247accd173174f64b6326a4078ba31e4b78146
nextdoorwith <nextdoorwithadm@gmail.com>
2023-06-10 20:50:48 +0900

    update property access example comments

--- 20230608_property_access/PropertyAccessExample/ExampleTest.cs
+++ 20230608_property_access/PropertyAccessExample/ExampleTest.cs
@@ -7,6 +7,9 @@ namespace PropertyAccessExample
 {
     public class ExampleTest
     {
+        /// <summary>
+        /// 各方式の測定回数
+        /// </summary>
         private const int TestCount = 3;
 
         private ITestOutputHelper _output;
@@ -16,13 +19,16 @@ namespace PropertyAccessExample
             _output = output;
         }
 
+        /// <summary>
+        /// 各方式の処理時間を測定する。
+        /// </summary>
         [Fact]
-        public void Test()
+        public void EvaluatePerformance()
         {
             var sw = new Stopwatch();
             var list = TestEntity.CreateTestEntityList(1_000_000);
 
-            // 評価対象のラムダ式
+            // 各方式のラムダ式
             var testType = new (string, Func<IEnumerable<TestEntity>, IEnumerable<string>>)[]
             {
                 ("switch", e => GetValuesBySwitch(list)),
@@ -32,7 +38,7 @@ namespace PropertyAccessExample
                 ("lamda list", e => GetValuesByLambdaList(list))
             };
 
-            // 各評価対象を複数回実行して処理時間を集計
+            // 各方式を複数回実行して処理時間を集計
             var sum = new long[testType.Length];
             for (var i = 0; i < TestCount; i++)
             {
@@ -51,7 +57,7 @@ namespace PropertyAccessExample
                 }
             }
 
-            // 評価対象毎に、集計した処理時間の平均を出力
+            // 各方式の平均処理時間を出力
             for (var i = 0; i < testType.Length; i++)
             {
                 var typeName = testType[i].Item1;
@@ -62,47 +68,51 @@ namespace PropertyAccessExample
 
         // ==================================================
 
-        public List<string> GetValuesBySwitch(IEnumerable<TestEntity> entityList)
+        /// <summary>
+        /// switchを使用したプロパティ値の取得方式
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static List<string> GetValuesBySwitch(IEnumerable<TestEntity> entityList)
         {
             var list = new List<string>();
             foreach (var entity in entityList)
             {
-                string val;
-                switch (entity.Selection)
+                string val = entity.Selection switch
                 {
-                    case "Prop01": val = entity.Prop01; break;
-                    case "Prop02": val = entity.Prop02; break;
-                    case "Prop03": val = entity.Prop03; break;
-                    case "Prop04": val = entity.Prop04; break;
-                    case "Prop05": val = entity.Prop05; break;
-                    case "Prop06": val = entity.Prop06; break;
-                    case "Prop07": val = entity.Prop07; break;
-                    case "Prop08": val = entity.Prop08; break;
-                    case "Prop09": val = entity.Prop09; break;
-                    case "Prop10": val = entity.Prop10; break;
-                    case "Prop11": val = entity.Prop11; break;
-                    case "Prop12": val = entity.Prop12; break;
-                    case "Prop13": val = entity.Prop13; break;
-                    case "Prop14": val = entity.Prop14; break;
-                    case "Prop15": val = entity.Prop15; break;
-                    case "Prop16": val = entity.Prop16; break;
-                    case "Prop17": val = entity.Prop17; break;
-                    case "Prop18": val = entity.Prop18; break;
-                    case "Prop19": val = entity.Prop19; break;
-                    case "Prop20": val = entity.Prop20; break;
-                    case "Prop21": val = entity.Prop21; break;
-                    case "Prop22": val = entity.Prop22; break;
-                    case "Prop23": val = entity.Prop23; break;
-                    case "Prop24": val = entity.Prop24; break;
-                    case "Prop25": val = entity.Prop25; break;
-                    case "Prop26": val = entity.Prop26; break;
-                    case "Prop27": val = entity.Prop27; break;
-                    case "Prop28": val = entity.Prop28; break;
-                    case "Prop29": val = entity.Prop29; break;
-                    case "Prop30": val = entity.Prop30; break;
-                    default:
-                        throw new InvalidOperationException();
-                }
+                    "Prop01" => entity.Prop01,
+                    "Prop02" => entity.Prop02,
+                    "Prop03" => entity.Prop03,
+                    "Prop04" => entity.Prop04,
+                    "Prop05" => entity.Prop05,
+                    "Prop06" => entity.Prop06,
+                    "Prop07" => entity.Prop07,
+                    "Prop08" => entity.Prop08,
+                    "Prop09" => entity.Prop09,
+                    "Prop10" => entity.Prop10,
+                    "Prop11" => entity.Prop11,
+                    "Prop12" => entity.Prop12,
+                    "Prop13" => entity.Prop13,
+                    "Prop14" => entity.Prop14,
+                    "Prop15" => entity.Prop15,
+                    "Prop16" => entity.Prop16,
+                    "Prop17" => entity.Prop17,
+                    "Prop18" => entity.Prop18,
+                    "Prop19" => entity.Prop19,
+                    "Prop20" => entity.Prop20,
+                    "Prop21" => entity.Prop21,
+                    "Prop22" => entity.Prop22,
+                    "Prop23" => entity.Prop23,
+                    "Prop24" => entity.Prop24,
+                    "Prop25" => entity.Prop25,
+                    "Prop26" => entity.Prop26,
+                    "Prop27" => entity.Prop27,
+                    "Prop28" => entity.Prop28,
+                    "Prop29" => entity.Prop29,
+                    "Prop30" => entity.Prop30,
+                    _ => throw new InvalidOperationException(),
+                };
                 list.Add(val);
             }
             return list;
@@ -110,7 +120,12 @@ namespace PropertyAccessExample
 
         // ==================================================
 
-        public List<string> GetValuesByTempDic(IEnumerable<TestEntity> entityList)
+        /// <summary>
+        /// Dictionaryを使用したプロパティ値の取得方式
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        public static List<string> GetValuesByTempDic(IEnumerable<TestEntity> entityList)
         {
             var list = new List<string>();
             foreach (var entity in entityList)
@@ -155,7 +170,12 @@ namespace PropertyAccessExample
 
         // ==================================================
 
-        public List<string> GetValuesByReflectionDic(IEnumerable<TestEntity> entityList)
+        /// <summary>
+        /// リフレクションを使用したプロパティ値の取得方式
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        public static List<string> GetValuesByReflectionDic(IEnumerable<TestEntity> entityList)
         {
             var list = new List<string>();
             foreach (var entity in entityList)
@@ -167,7 +187,7 @@ namespace PropertyAccessExample
             return list;
         }
 
-        private Dictionary<string, PropertyInfo?> refDic = new()
+        private static Dictionary<string, PropertyInfo?> refDic = new()
         {
             ["Prop01"] = typeof(TestEntity).GetProperty("Prop01"),
             ["Prop02"] = typeof(TestEntity).GetProperty("Prop02"),
@@ -203,7 +223,12 @@ namespace PropertyAccessExample
 
         // ==================================================
 
-        public List<string> GetValuesByLambdaDic(IEnumerable<TestEntity> entityList)
+        /// <summary>
+        /// ラムダ式Dictionaryを使用したプロパティ値の取得方式
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        public static List<string> GetValuesByLambdaDic(IEnumerable<TestEntity> entityList)
         {
             var list = new List<string>();
             foreach (var entity in entityList)
@@ -214,7 +239,7 @@ namespace PropertyAccessExample
             return list;
         }
 
-        private readonly Dictionary<string, Func<TestEntity, string>> lambdaDic = new()
+        private static readonly Dictionary<string, Func<TestEntity, string>> lambdaDic = new()
         {
             ["Prop01"] = e => e.Prop01,
             ["Prop02"] = e => e.Prop02,
@@ -250,7 +275,23 @@ namespace PropertyAccessExample
 
         // ==================================================
 
-        private readonly List<Func<TestEntity, string>> lambdaList = new()
+        /// <summary>
+        /// ラムダ式リストを使用したプロパティ値の取得方式
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        public static List<string> GetValuesByLambdaList(IEnumerable<TestEntity> entityList)
+        {
+            var list = new List<string>();
+            foreach (var entity in entityList)
+            {
+                var getter = lambdaList[(int)entity.Selction2];
+                list.Add(getter(entity));
+            }
+            return list;
+        }
+
+        private static readonly List<Func<TestEntity, string>> lambdaList = new()
         {
             e => e.Prop01,
             e => e.Prop02,
@@ -283,18 +324,6 @@ namespace PropertyAccessExample
             e => e.Prop29,
             e => e.Prop30
         };
-
-        public List<string> GetValuesByLambdaList(IEnumerable<TestEntity> entityList)
-        {
-            var list = new List<string>();
-            foreach (var entity in entityList)
-            {
-                var getter = lambdaList[(int)entity.Selction2];
-                list.Add(getter(entity));
-            }
-            return list;
-        }
-
     }
 
 
